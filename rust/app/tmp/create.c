98f7a870be5b20786b953496191d18c1145f939a
lupyuen <luppy@appkaki.com>
2020-03-13 19:26:20 +0800

    Updated doc

--- rust/app/tmp/create.c
+++ rust/app/tmp/create.c
@@ -1,20 +1,11 @@
 `lv_obj_t *screen_time_create(home_time_widget_t *ht) {` <br>
 &nbsp;&nbsp;`    //  Create a label for time (00:00)` <br>
 &nbsp;&nbsp;`    lv_obj_t *scr = lv_obj_create(NULL, NULL);` <br>
-&nbsp;&nbsp;`    lv_obj_t * label1 = lv_label_create(scr, NULL);` <br>
-&nbsp;&nbsp;`    lv_label_set_long_mode(label1, LV_LABEL_LONG_BREAK);` <br>
+&nbsp;&nbsp;`    lv_obj_t *label1 = lv_label_create(scr, NULL);` <br>
 &nbsp;&nbsp;`    lv_label_set_text(label1, "00:00");` <br>
 &nbsp;&nbsp;`    lv_obj_set_width(label1, 240);` <br>
 &nbsp;&nbsp;`    lv_obj_set_height(label1, 200);` <br>
-&nbsp;&nbsp;`    lv_label_set_align(label1, LV_LABEL_ALIGN_CENTER);` <br>
-&nbsp;&nbsp;`    lv_obj_align(label1, scr, LV_ALIGN_CENTER, 0, -30);` <br>
-&nbsp;&nbsp;`    lv_label_set_style(label1, LV_LABEL_STYLE_MAIN, &style_time);` <br>
 &nbsp;&nbsp;`    ht->lv_time = label1;` <br>
 &nbsp;&nbsp;`    ...` <br>
-&nbsp;&nbsp;`    //  Set the touch callback` <br>
-&nbsp;&nbsp;`    lv_obj_set_event_cb(scr, _screen_time_pressed);` <br>
-&nbsp;&nbsp;`    //  Update the widgets` <br>
-&nbsp;&nbsp;`    _screen_time_update_screen(&ht->widget);` <br>
-&nbsp;&nbsp;`    //  Return the screen` <br>
 &nbsp;&nbsp;`    return scr;` <br>
 `}` <br>
--- rust/app/tmp/create.rs
+++ rust/app/tmp/create.rs
@@ -3,15 +3,10 @@
 &nbsp;&nbsp;`    //  Create a label for time (00:00)` <br>
 &nbsp;&nbsp;`    let scr = widgets.screen;` <br>
 &nbsp;&nbsp;`    let label1 = label::create(scr, ptr::null()) ? ;` <br>
-&nbsp;&nbsp;`    label::set_long_mode(label1, label::LV_LABEL_LONG_BREAK) ? ;` <br>
 &nbsp;&nbsp;`    label::set_text(label1, strn!("00:00")) ? ;` <br>
 &nbsp;&nbsp;`    obj::set_width(label1, 240) ? ;` <br>
 &nbsp;&nbsp;`    obj::set_height(label1, 200) ? ;` <br>
-&nbsp;&nbsp;`    label::set_align(label1, label::LV_LABEL_ALIGN_CENTER) ? ;` <br>
-&nbsp;&nbsp;`    obj::align(label1, scr, obj::LV_ALIGN_CENTER, 0, -30) ? ;` <br>
-&nbsp;&nbsp;`    obj::set_style(label1, unsafe { &style_time }) ? ;` <br>
 &nbsp;&nbsp;`    widgets.time_label = label1;` <br>
 &nbsp;&nbsp;`    ...` <br>
-&nbsp;&nbsp;`    //  Return OK` <br>
 &nbsp;&nbsp;`    Ok(())` <br>
 `}` <br>
